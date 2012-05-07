7bca7664d38a9940a2c8c58f7c4fa4364ff3cd6f
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2012-05-07 14:48:33 +0200

    Add feature to enforce absence of a sudo group

--- manifests/group.pp
+++ manifests/group.pp
@@ -1,12 +1,23 @@
-define sudo::group (ensure='present', $group=$title, $host_group='ALL', $command='ALL', $runas_user='ALL') {
-    augeas { "sudoers-grp-${group}":
-        context => "/files/etc/sudoers",
-        changes => [
-            "set spec[user = '%${group}']/user %${group}",
-            "set spec[user = '%${group}']/host_group/host ${host_group}",
-            "set spec[user = '%${group}']/host_group/command ${command}",
-            "set spec[user = '%${group}']/host_group/command/runas_user ${runas_user}",
-        ],
+define sudo::group ($ensure='present', $group=$title, $host_group='ALL', $command='ALL', $runas_user='ALL') {
+
+    if $ensure == 'present' {
+        augeas { "sudoers-grp-${group}":
+            context => "/files/etc/sudoers",
+            changes => [
+                "set spec[user = '%${group}']/user %${group}",
+                "set spec[user = '%${group}']/host_group/host ${host_group}",
+                "set spec[user = '%${group}']/host_group/command ${command}",
+                "set spec[user = '%${group}']/host_group/command/runas_user ${runas_user}",
+            ],
+
+        }
+    } elsif $ensure == 'absent' {
+        augeas { "sudoers-grp-${group}":
+            context => "/files/etc/sudoers",
+            changes => [
+                "rm spec[*][user = '%${group}']"
+            ]
+        }
 
     }
 }
