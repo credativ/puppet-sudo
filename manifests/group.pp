eeaccac50ec6205fc753a196be07be5e9e0e0485
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-05-25 23:05:03 +0200

    Fixed several issues detected by puppet-lint

--- manifests/group.pp
+++ manifests/group.pp
@@ -1,23 +1,27 @@
-define sudo::group ($ensure='present', $group=$title, $host_group='ALL', $command='ALL', $runas_user='ALL') {
+define sudo::group (
+  $ensure = 'present',
+  $group = $title,
+  $host_group = 'ALL',
+  $command='ALL',
+  $runas_user='ALL'
+) {
 
-    if $ensure == 'present' {
-        augeas { "sudoers-grp-${group}":
-            context => "/files/etc/sudoers",
-            changes => [
-                "set spec[user = '%${group}']/user %${group}",
-                "set spec[user = '%${group}']/host_group/host ${host_group}",
-                "set spec[user = '%${group}']/host_group/command ${command}",
-                "set spec[user = '%${group}']/host_group/command/runas_user ${runas_user}",
-            ],
-
-        }
-    } elsif $ensure == 'absent' {
-        augeas { "sudoers-grp-${group}":
-            context => "/files/etc/sudoers",
-            changes => [
-                "rm spec[*][user = '%${group}']"
-            ]
-        }
-
+  if $ensure == 'present' {
+    augeas { "sudoers-grp-${group}":
+      context => '/files/etc/sudoers',
+      changes => [
+        "set spec[user = '%${group}']/user %${group}",
+        "set spec[user = '%${group}']/host_group/host ${host_group}",
+        "set spec[user = '%${group}']/host_group/command ${command}",
+        "set spec[user = '%${group}']/host_group/command/runas_user ${runas_user}",
+      ],
     }
+  } elsif $ensure == 'absent' {
+    augeas { "sudoers-grp-${group}":
+      context => '/files/etc/sudoers',
+      changes => [
+        "rm spec[*][user = '%${group}']"
+      ]
+    }
+  }
 }
