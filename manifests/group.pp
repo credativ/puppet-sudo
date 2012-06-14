ec4177fb7aab001509417fb7c6f7f52b946dc488
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2012-06-14 09:11:17 +0200

    Add documentation for sudo::group

--- manifests/group.pp
+++ manifests/group.pp
@@ -1,3 +1,28 @@
+# = Class: sudo::group
+#
+# Define to handle a group entry in /etc/sudoers
+#
+# == Parameters:
+#
+# [* ensure *]
+#   weither sudo group shall be ensured to be 'present' or 'absent'
+#
+# [* group *]
+#   specify the group name (defaults to: $title)
+#
+# [* host_group *]
+#   specify the host_group of the sudo rule (see sudoers for an explanation
+#   of Host_Aliases and their usage.
+#   Default: ALL
+#
+# [* command *]
+#   allows to specify whiche command the group may run
+#   Default: ALL
+#
+# [* runas_user *]
+#   specify which user the group may switch to.
+#   Default: ALL
+#
 define sudo::group (
   $ensure = 'present',
   $group = $title,
