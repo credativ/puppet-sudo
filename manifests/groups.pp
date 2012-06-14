21f0b9150505ebb1399758b630acde9f8736fd12
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2012-06-14 09:02:29 +0200

    Add some documentation to sudo::groups

--- manifests/groups.pp
+++ manifests/groups.pp
@@ -1,6 +1,14 @@
+# = Class: sudo::groups
+#
+# Create a bunch of sudo::group resources from a given hash of hashes with
+# name_of_group => group_attribute pairs.
+#
+# See sudo::group for further information.
+#
+
 class sudo::groups (
     $ensure,
-    $groups,
+    $groups = undef,
     ) {
 
     if $ensure {
