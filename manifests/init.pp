b8aecf1d50e9ddd6bc70a421a549aefa3c3daa2c
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2012-06-14 08:59:09 +0200

    Add documentation to sudo class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,25 @@
+# = Class: sudo
+#
+# Ensure and manage group entries in the sudoers file
+#
+# == Parameters
+#
+# [* manage_groups *]
+#   Weither to manage sudo groups or not. Default: true
+#
+# [* groups *]
+#   Specify the groups the sudo module shall manage. Needs to be
+#   a hash of hashes, with groupname as key and a hash describing the
+#   group as value. See the sudo::group documentation for details about
+#   the allowed keys and defaults.
+#         
+# == Usage
+# 
+#
+# class {'sudo':
+#   groups  => { "sudo_users": {} }
+#
+
 class sudo (
     $ensure         = params_lookup('ensure'),
     $manage_groups  = params_lookup('manage_groups'),
